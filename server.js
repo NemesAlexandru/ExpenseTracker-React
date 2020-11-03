f0107fe756fe74adb7be8713d20424e2846433bd
Nemes Alex <54545364+NemesAlexandru@users.noreply.github.com>
2020-11-03 16:20:57 +0200

    MongoDB server-side connection

--- server.js
+++ server.js
@@ -1,6 +1,40 @@
+//express
+
 const express = require('express');
 const app = express();
 
+//mongoose
+
+const mongoose = require('mongoose');
+
+//body-parser
+
+var bodyParser = require('body-parser');
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//.env
+
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+  };
+
+  //DB config
+
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const db = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
+
+//Connect to Mongo
+
+mongoose.connect(db, { useUnifiedTopology: true, 
+    useNewUrlParser: true }).then(() => 
+    console.log('MongoDB connected...')).catch(err => console.log(err));
+
+  //test
+  
 app.get('/api/customers', (req, res) =>{
     const customers = [
     {id: 1, firstName: 'John', lastName: 'Doe'},
@@ -10,6 +44,6 @@ app.get('/api/customers', (req, res) =>{
     res.json(customers);
 });
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server started on port ${port}`));
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
